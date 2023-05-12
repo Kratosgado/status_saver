4be66d4d7138d1dd3e8bffb735b5418c6aaf301d
Prince Mbeah Essilfie <pmessilfie@st.knust.edu.gh>
2023-05-12 12:06:09 -0700

    flutter_file_manager removed, it doesn't support sound null safety

--- lib/src/media_folder.dart
+++ lib/src/media_folder.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_file_manager/flutter_file_manager.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:video_player/video_player.dart';
@@ -27,9 +26,10 @@ class _MediaFolderViewState extends State<MediaFolderView> {
   Future<void> loadFiles() async {
     final folder = Directory(widget.folderPath);
     if (await folder.exists()) {
-      final filesInFolder = await FileManager.listFiles(widget.folderPath);
+      final folderContents = folder.listSync();
+      final filePaths = folderContents.whereType<File>().map((file) => file.path).toList();
       setState(() {
-        files = filesInFolder;
+        files = filePaths.map((path) => File(path)).toList();
       });
     }
   }
@@ -42,13 +42,13 @@ class _MediaFolderViewState extends State<MediaFolderView> {
       ),
       body: files.isEmpty
           ? const Center(child: CircularProgressIndicator())
-          : SliverAnimatedGrid(
+          : GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 2,
               ),
-              initialItemCount: files.length,
-              itemBuilder: (BuildContext context, int index, animatedDouble) {
+              itemCount: files.length,
+              itemBuilder: (BuildContext context, int index) {
                 final file = files[index];
                 final extension = file.path.split('.').last.toLowerCase();
                 if (extension == 'mp4') {
@@ -59,7 +59,6 @@ class _MediaFolderViewState extends State<MediaFolderView> {
                   return Container();
                 }
               },
-              // staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
             ),
     );
   }
@@ -100,3 +99,107 @@ class _MediaFolderViewState extends State<MediaFolderView> {
     );
   }
 }
+
+
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_file_manager/flutter_file_manager.dart';
+// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:video_player/video_player.dart';
+
+// class MediaFolderView extends StatefulWidget {
+//   final String folderPath;
+
+//   const MediaFolderView({Key? key, required this.folderPath}) : super(key: key);
+
+//   @override
+//   _MediaFolderViewState createState() => _MediaFolderViewState();
+// }
+
+// class _MediaFolderViewState extends State<MediaFolderView> {
+//   late List<File> files;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     loadFiles();
+//   }
+
+//   Future<void> loadFiles() async {
+//     final folder = Directory(widget.folderPath);
+//     if (await folder.exists()) {
+//       final filesInFolder = await FileManager.listFiles(widget.folderPath);
+//       setState(() {
+//         files = filesInFolder;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Media Folder'),
+//       ),
+//       body: files.isEmpty
+//           ? const Center(child: CircularProgressIndicator())
+//           : SliverAnimatedGrid(
+//               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//                 crossAxisCount: 2,
+//                 crossAxisSpacing: 2,
+//               ),
+//               initialItemCount: files.length,
+//               itemBuilder: (BuildContext context, int index, animatedDouble) {
+//                 final file = files[index];
+//                 final extension = file.path.split('.').last.toLowerCase();
+//                 if (extension == 'mp4') {
+//                   return _buildVideoItem(file);
+//                 } else if (extension == 'jpg' || extension == 'jpeg' || extension == 'png') {
+//                   return _buildImageItem(file);
+//                 } else {
+//                   return Container();
+//                 }
+//               },
+//               // staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
+//             ),
+//     );
+//   }
+
+//   Widget _buildImageItem(File file) {
+//     return InkWell(
+//       onTap: () {
+//         // Handle image tap
+//       },
+//       child: CachedNetworkImage(
+//         imageUrl: file.path,
+//         fit: BoxFit.cover,
+//         placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+//         errorWidget: (context, url, error) => const Icon(Icons.error),
+//       ),
+//     );
+//   }
+
+//   Widget _buildVideoItem(File file) {
+//     final videoPlayerController = VideoPlayerController.file(file);
+//     return FutureBuilder(
+//       future: videoPlayerController.initialize(),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.done) {
+//           return InkWell(
+//             onTap: () {
+//               videoPlayerController.play();
+//             },
+//             child: AspectRatio(
+//               aspectRatio: videoPlayerController.value.aspectRatio,
+//               child: VideoPlayer(videoPlayerController),
+//             ),
+//           );
+//         } else {
+//           return Container();
+//         }
+//       },
+//     );
+//   }
+// }
